23f90f8fb11804c622b4ee724f16e927be72aa47
sabrek15 <reddy.ksb007@gmail.com>
2025-03-04 17:35:44 +0530

    added markdown to html node function

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,5 +1,6 @@
 import re
 from enum import Enum
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
@@ -31,3 +32,46 @@ def block_to_block_type(block):
         return BlockType.ORDERED_LIST
     else:
         return BlockType.PARAGRAPH
+    
+def convert_inline_markdown(text):
+    text = re.sub(r"\*\*(.*?)\*\*", r"<b>\1</b>", text)  # **bold** -> <b>bold</b>
+    text = re.sub(r"\_(.*?)\_", r"<i>\1</i>", text)      # _italic_ -> <i>italic</i>
+    text = re.sub(r"\`(.*?)\`", r"<code>\1</code>", text) # `code` -> <code>code</code>
+    return text
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    children = []
+
+    for block in blocks:
+        blocktype = block_to_block_type(block)
+        
+        if blocktype == BlockType.HEADING:
+            for line in block.split('\n'):
+                level = len(line) - len(line.lstrip("#"))
+                tag = f"h{level}"
+                value = line[level:].strip()
+                children.append(LeafNode(tag, convert_inline_markdown(value), None))
+
+        elif blocktype == BlockType.CODE:
+            value = block[3:-3]
+            children.append(LeafNode("pre", convert_inline_markdown(value), None))
+
+        elif blocktype == BlockType.QUOTE:
+            tag = "blockquote"
+            value = " ".join([line.lstrip("> ").strip() for line in block.split('\n')])
+            children.append(LeafNode(tag, convert_inline_markdown(value)))
+
+        elif blocktype == BlockType.ORDERED_LIST:
+            list_items = [LeafNode("li", convert_inline_markdown(line[2:].strip())) for line in block.split('\n')]
+            children.append(ParentNode("ol", list_items))
+
+        elif blocktype == BlockType.UNORDERED_LIST:
+            list_items = [LeafNode("li", convert_inline_markdown(line[2:].strip())) for line in block.split('\n')]
+            children.append(ParentNode("ul", list_items))
+
+        else:
+            paragraph_text = " ".join(block.split('\n')).strip()
+            children.append(LeafNode("p", convert_inline_markdown(paragraph_text)))
+
+    return ParentNode("div", children)
